062a3963d01164d1a6bf180fd42c4eb6388d1021
negipo <negipo@gmail.com>
2014-12-19 22:56:40 +0900

    Add omniauth

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Donotremembercardnumber
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    config.action_controller.permit_all_parameters = true
   end
 end
