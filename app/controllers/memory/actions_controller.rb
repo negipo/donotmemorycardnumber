4284917fadc1b47806ec030db38a67110e0f8a3c
negipo <negipo@gmail.com>
2014-12-23 17:39:05 +0900

    Add lists

--- app/controllers/memory/actions_controller.rb
+++ app/controllers/memory/actions_controller.rb
@@ -1,6 +1,10 @@
 class Memory::ActionsController < ApplicationController
   def index
-    first_number, last_number = params[:range].split('-').map(&:to_i)
-    @actions = Memory::Action.at(first_number..last_number)
+    if params[:range]
+      first_number, last_number = params[:range].split('-').map(&:to_i)
+      @actions = Memory::Action.at(first_number..last_number)
+    else
+      @actions = Memory::Action.all
+    end
   end
 end
--- app/controllers/memory/objects_controller.rb
+++ app/controllers/memory/objects_controller.rb
@@ -1,6 +1,10 @@
 class Memory::ObjectsController < ApplicationController
   def index
-    first_number, last_number = params[:range].split('-').map(&:to_i)
-    @objects = Memory::Object.at(first_number..last_number)
+    if params[:range]
+      first_number, last_number = params[:range].split('-').map(&:to_i)
+      @objects = Memory::Object.at(first_number..last_number)
+    else
+      @objects = Memory::Object.all
+    end
   end
 end
